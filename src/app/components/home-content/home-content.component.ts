c831e3dd4e438aa043bebb8f81bbcc1feeb2a212
Vennela Gonuguntla <vgonuguntla@oakland.edu>
2023-03-20 20:00:48 -0400

    slider images

--- src/app/components/home-content/home-content.component.ts
+++ src/app/components/home-content/home-content.component.ts
@@ -12,35 +12,90 @@ export class HomeContentComponent {
   slider!: NgImageSliderComponent;
 
   imageObject = [{
-    image: 'https://drive.google.com/drive/folders/1ke4BT2vcpZLyytk-BXGs9Ez0Z7zEBBOV/img_1.jpg',
-    thumbImage: 'https://drive.google.com/drive/folders/1ke4BT2vcpZLyytk-BXGs9Ez0Z7zEBBOV/img_1.jpg',
-    title: 'Hummingbirds are amazing creatures'
-}, {
-    image: 'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/9.jpg',
-    thumbImage: 'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/9.jpg'
-}, {
-    image: 'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/4.jpg',
-    thumbImage: 'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/4.jpg',
-    title: 'Example with title.'
-},{
-    image: 'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/7.jpg',
-    thumbImage: 'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/7.jpg',
-    title: 'Hummingbirds are amazing creatures'
-}, {
-    image: 'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/1.jpg',
-    thumbImage: 'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/1.jpg'
-}, {
-    image: 'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/2.jpg',
-    thumbImage: 'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/2.jpg',
-    title: 'Example two with title.'
-}];
+    images: [{
+      image: 'https://raw.githubusercontent.com/vennela9801/real_estate_angular15/main/src/assets/houses/img_1.jpg',
+      thumbImage: 'https://raw.githubusercontent.com/vennela9801/real_estate_angular15/main/src/assets/houses/img_1.jpg',
+      title: 'Hummingbirds are amazing creatures'
+    }, {
+      image: 'https://raw.githubusercontent.com/vennela9801/real_estate_angular15/main/src/assets/houses/img_2.jpg',
+      thumbImage: 'https://raw.githubusercontent.com/vennela9801/real_estate_angular15/main/src/assets/houses/img_2.jpg',
+    }, {
+      image: 'https://raw.githubusercontent.com/vennela9801/real_estate_angular15/main/src/assets/houses/img_3.jpg',
+      thumbImage: 'https://raw.githubusercontent.com/vennela9801/real_estate_angular15/main/src/assets/houses/img_3.jpg',
+      title: 'Example with title.'
+    }, {
+      image: 'https://raw.githubusercontent.com/vennela9801/real_estate_angular15/main/src/assets/houses/img_4.jpg',
+      thumbImage: 'https://raw.githubusercontent.com/vennela9801/real_estate_angular15/main/src/assets/houses/img_4.jpg',
+      title: 'Hummingbirds are amazing creatures'
+    }, {
+      image: 'https://raw.githubusercontent.com/vennela9801/real_estate_angular15/main/src/assets/houses/img_5.jpg',
+      thumbImage: 'https://raw.githubusercontent.com/vennela9801/real_estate_angular15/main/src/assets/houses/img_5.jpg',
+    }],
+    title: 'Mansion',
+    location: 'Novi, Mi 48836',
+    yearBuild: '1990',
+    rate: '$45,789.00'
+
+  },
+  {
+    images: [{
+      image: 'https://raw.githubusercontent.com/vennela9801/real_estate_angular15/main/src/assets/houses/img_6.jpg',
+      thumbImage: 'https://raw.githubusercontent.com/vennela9801/real_estate_angular15/main/src/assets/houses/img_6.jpg',
+      title: 'Hummingbirds are amazing creatures'
+    }, {
+      image: 'https://raw.githubusercontent.com/vennela9801/real_estate_angular15/main/src/assets/houses/img_7.jpg',
+      thumbImage: 'https://raw.githubusercontent.com/vennela9801/real_estate_angular15/main/src/assets/houses/img_7.jpg',
+    }, {
+      image: 'https://raw.githubusercontent.com/vennela9801/real_estate_angular15/main/src/assets/houses/img_8.jpg',
+      thumbImage: 'https://raw.githubusercontent.com/vennela9801/real_estate_angular15/main/src/assets/houses/img_8.jpg',
+      title: 'Example with title.'
+    }, {
+      image: 'https://raw.githubusercontent.com/vennela9801/real_estate_angular15/main/src/assets/houses/img_9.jpg',
+      thumbImage: 'https://raw.githubusercontent.com/vennela9801/real_estate_angular15/main/src/assets/houses/img_9.jpg',
+      title: 'Hummingbirds are amazing creatures'
+    }, {
+      image: 'https://raw.githubusercontent.com/vennela9801/real_estate_angular15/main/src/assets/houses/img_10.jpg',
+      thumbImage: 'https://raw.githubusercontent.com/vennela9801/real_estate_angular15/main/src/assets/houses/img_10.jpg',
+    }],
+    title: 'Brick House',
+    location: 'Farmington Hills, Mi 48839',
+    yearBuild: '2014',
+    rate: '$97,450.00'
+
+  },
+  {
+    images: [{
+      image: 'https://raw.githubusercontent.com/vennela9801/real_estate_angular15/main/src/assets/houses/img_11.jpg',
+      thumbImage: 'https://raw.githubusercontent.com/vennela9801/real_estate_angular15/main/src/assets/houses/img_11.jpg',
+      title: 'Hummingbirds are amazing creatures'
+    }, {
+      image: 'https://raw.githubusercontent.com/vennela9801/real_estate_angular15/main/src/assets/houses/img_12.jpg',
+      thumbImage: 'https://raw.githubusercontent.com/vennela9801/real_estate_angular15/main/src/assets/houses/img_12.jpg',
+    }, {
+      image: 'https://raw.githubusercontent.com/vennela9801/real_estate_angular15/main/src/assets/houses/img_13.jpeg',
+      thumbImage: 'https://raw.githubusercontent.com/vennela9801/real_estate_angular15/main/src/assets/houses/img_13.jpeg',
+      title: 'Example with title.'
+    }, {
+      image: 'https://raw.githubusercontent.com/vennela9801/real_estate_angular15/main/src/assets/houses/img_14.jpeg',
+      thumbImage: 'https://raw.githubusercontent.com/vennela9801/real_estate_angular15/main/src/assets/houses/img_14.jpeg',
+      title: 'Hummingbirds are amazing creatures'
+    }, {
+      image: 'https://raw.githubusercontent.com/vennela9801/real_estate_angular15/main/src/assets/houses/img_15.jpg',
+      thumbImage: 'https://raw.githubusercontent.com/vennela9801/real_estate_angular15/main/src/assets/houses/img_15.jpg',
+    }],
+    title: 'Brick House',
+    location: 'Farmington Hills, Mi 48839',
+    yearBuild: '2014',
+    rate: '$97,450.00'
+
+  }];
 
 
-    prevImageClick() {
-        this.slider.prev();
-    }
+  prevImageClick() {
+    this.slider.prev();
+  }
 
-    nextImageClick() {
-        this.slider.next();
-    }
+  nextImageClick() {
+    this.slider.next();
+  }
 }
